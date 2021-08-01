a39622c0ef3fb5bd17e38fa9157cee57b0af0316
jianghu52 <jianghu52@gmail.com>
2021-08-01 19:24:43 +0800

    new add

--- src/sales/views.py
+++ src/sales/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Sale
 # Create your views here.
+
+def home_view(request):
+    hello = 'now time'
+    return render(request, 'sales/home.html', {'h':hello})
+
+class SaleListView(ListView):
+    model = Sale
+    template_name = 'sales/main.html'
