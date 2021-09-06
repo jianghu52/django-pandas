54c599066783cd05ef473540f74ee95a9f66c7bf
jianghu52 <jianghu52@gmail.com>
2021-09-06 21:38:21 +0800

    new add

--- src/sales/urls.py
+++ src/sales/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
-from .views import (home_view,SaleListView)
+from .views import (home_view,SaleListView,SaleDetailView,)
 
 app_name = 'sales'
 
 urlpatterns =[
     path('', home_view, name='home'),
-    path('list/', SaleListView.as_view(), name='list')
+    path('sales/', SaleListView.as_view(), name='list'),
+    path('sales/<pk>/', SaleDetailView.as_view(), name='detail')
 ]
--- src/sales/views.py
+++ src/sales/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Sale
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import  SalesSearchForm
+
 # Create your views here.
 
 def home_view(request):
-    hello = 'now time'
-    return render(request, 'sales/home.html', {'h':hello})
+    form = SalesSearchForm(request.POST or None)
+    if request.method == 'POST':
+        data_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_types = request.POST.get('chart_type')
+    context = {
+        'form':form
+    }
+    return render(request, 'sales/home.html', context)
 
-class SaleListView(ListView):
+
+class SaleListView( ListView):
     model = Sale
     template_name = 'sales/main.html'
+
+
+class SaleDetailView(DetailView):
+    model = Sale
+    template_name = 'sales/detail.html'
+
+def sale_list_view(request):
+    qs = Sale.objects.all()
+    return render(request, 'sales/main.html', {'qs':qs})
+
+def sale_detail_view(request,**kwargs):
+    pk = kwargs.get('pk')
+    obj = Sale.objects.get(pk=pk)
+    return render(request, 'sales/detail.html', {'object':obj})
