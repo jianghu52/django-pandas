4b1bc4b6e0d492ceb0c7541de6c9fbcf1ee22a43
jianghu52 <jianghu52@gmail.com>
2021-10-30 22:41:00 +0800

    urlpatterns 的顺序很重要，尤其是path('<pk>/'）要放在最后面，不然会优先执行这个，而不是执行save/。导致了ajax 执行失败

--- src/reports/urls.py
+++ src/reports/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 
-from .views import(
+from .views import (
     create_report_view,
     ReportListView,
     ReportDetailView
-) 
-
+)
 
 app_name = 'reports'
 
 urlpatterns = [
-    path('',ReportListView.as_view(),name='main'),
-    path('<pk>/',ReportDetailView.as_view(),name='detail'),
-    path('save/',create_report_view,name='create-report'),
-    ]
+    path('', ReportListView.as_view(), name='main'),
+    path('save/', create_report_view, name='create-report'),
+    path('<pk>/', ReportDetailView.as_view(), name='detail'),
+
+]
